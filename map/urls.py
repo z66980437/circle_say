e881918286052782438619dbe2ba00abda00574c
yuxiaoyu <1044869221@qq.com>
2019-02-28 10:07:02 +0800

    地图查市

--- map/urls.py
+++ map/urls.py
@@ -1,8 +1,10 @@
 from django.urls import path
 
 from map import views
+from map.views import CityView
 
 urlpatterns = [
     # 测试
-    path(r'hello/', views.hello, name='hello'),
+    path('cities/', CityView.as_view(), name='cities'),
+    path('cities/<int:pk>/', CityView.as_view(), name='city'),
 ]
