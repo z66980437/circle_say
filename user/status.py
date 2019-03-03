c4eb29769f10d3cd66d99e660ebeb6b73d25c3c9
wildpigger <66980437@qq.com>
2019-03-03 15:33:24 +0800

    添加用户登录，注册，查好友，删好友功能

--- user/status.py
+++ user/status.py
@@ -2,10 +2,9 @@
 
 # Login Function
 LOGIN_SUCCESS = {'code':200, 'msg': '登录成功'}
-PASSWORD_ERROR = {'code': 100001, 'msg': '用户密码不正确'}
-USERNAME_NOT_EXIST = {'code': 100002, 'msg': '用户不存在， 请注册'}
+LOGIN_ERROR = {'code': 100001, 'msg': '用户名或密码不正确'}
 
 # Register Function
 REGISTER_SUCCESS = {'code': 200, 'msg': '注册成功'}
-USERNAME_EXIST = {'code': 100003, 'msg': '该用户名已被注册，请去登录'}
-FORM_ERRORS = {'code': 100004}
+USERNAME_EXIST = {'code': 100002, 'msg': '该用户名已被注册，请去登录'}
+FORM_ERRORS = {'code': 100003}
--- user/views.py
+++ user/views.py
@@ -1,13 +1,22 @@
+import pickle
+from uuid import uuid1
+
+from django.db import connections, transaction
+from django.db.models import QuerySet
 from django.http import JsonResponse
 
 # Create your views here.
 from django.utils import timezone
+from django_redis import get_redis_connection
 from rest_framework import mixins
 from rest_framework.decorators import api_view
+from rest_framework.response import Response
+from rest_framework.status import HTTP_204_NO_CONTENT
 from rest_framework.viewsets import GenericViewSet
 
 from user.forms import UserRegisterForm
-from user.models import User, LoginLog
+from user.helpers import CustomAuthentication
+from user.models import User, LoginLog, Relation
 from user.serializers import UserSerializer
 from user.utils import to_md5_hex, get_ip_address
 from user.status import *
@@ -18,13 +27,12 @@ def UserLogin(request):
     # 获取POST参数
     username = request.POST.get('username')
     password = request.POST.get('password')
-    if username and password:
-        # 获取用户信息
-        user = User.objects.filter(username=username).first()
-        if not user:
-            return JsonResponse(USERNAME_NOT_EXIST)
-        if to_md5_hex(password) != user.password:
-            return JsonResponse(PASSWORD_ERROR)
+    # 获取用户信息
+    user = User.objects.filter(username=username, password=to_md5_hex(password)).first()
+    if not user:
+        return JsonResponse(LOGIN_ERROR)
+    # 事务环境  或者在视图函数中加上@atomic装饰器， 导入地方跟下面一样
+    with transaction.atomic():
         # 登录成功，进行后续操作
         now_time = timezone.now().strftime('%Y-%m-%d %H:%M:%S')
         user.lastvisit = now_time
@@ -34,7 +42,10 @@ def UserLogin(request):
         new_login_log.ipaddr = get_ip_address(request)
         new_login_log.logdate = now_time
         new_login_log.save()
-        return JsonResponse(LOGIN_SUCCESS)
+    LOGIN_SUCCESS['token'] = uuid1().hex
+    redis_client = get_redis_connection()
+    redis_client.set(LOGIN_SUCCESS['token'], pickle.dumps(user), 3600)
+    return JsonResponse(LOGIN_SUCCESS)
 
 
 @api_view(['POST'])
@@ -70,6 +81,9 @@ def UserRegister(request):
         new_login_log.ipaddr = get_ip_address(request)
         new_login_log.logdate = now_time
         new_login_log.save()
+        REGISTER_SUCCESS['token'] = uuid1().hex
+        redis_client = get_redis_connection()
+        redis_client.set(LOGIN_SUCCESS['token'], pickle.dumps(user), 3600)
         return JsonResponse(REGISTER_SUCCESS)
     else:
         FORM_ERRORS['msg'] = form_user.errors
@@ -79,16 +93,65 @@ def UserRegister(request):
 class FriendViewSet(mixins.CreateModelMixin,
                    mixins.RetrieveModelMixin,
                    # mixins.UpdateModelMixin,
-                   # mixins.DestroyModelMixin,
+                   mixins.DestroyModelMixin,
                    mixins.ListModelMixin,
                    GenericViewSet):
     serializer_class = UserSerializer
+    authentication_classes = (CustomAuthentication,)
 
     def get_queryset(self):
-        queryset = User.objects.filter(user_id=9).select_related('region').first()
+        user = self.request.user
+        friends = Relation.objects.filter(user_id=user.user_id, is_delete=False).only('friend').select_related('friend')
+        friends_list = [item.friend_id for item in friends]
+        queryset = User.objects.filter(user_id__in=friends_list).all()
         return queryset
 
+    def create(self, request, *args, **kwargs):
+        user = request.user
+        friend_id = request.POST.get('friend_id', '')
+        content = request.POST.get('content', '')
+        friend = User.objects.filter(user_id=friend_id).first()
+        if friend:
+            new_relation = Relation(user=user, friend=friend, is_delete=False, is_show=False)
+            new_relation.save()
+            return JsonResponse({
+                'code': 200,
+                'msg': '已发送添加申请！'
+            })
+        else:
+            return JsonResponse({
+                'code': 10004,
+                'msg': '用户不存在！'
+            })
+    def perform_create(self, serializer):
+        pass
 
+    def get_success_headers(self, data):
+        pass
+
+    def retrieve(self, request, *args, **kwargs):
+        instance = User.objects.filter(user_id=kwargs['pk'], is_delete=False).first()
+        if instance:
+            serializer = UserSerializer(instance)
+            return Response(serializer.data)
+        else:
+            return JsonResponse({
+                'code': 10004,
+                'msg': '用户不存在！'
+            })
+
+    def destroy(self, request, *args, **kwargs):
+        user = request.user
+        relation = Relation.objects.filter(user_id=user.user_id, friend_id=kwargs['pk']) .first()
+        if relation and not relation.is_delete:
+            relation.is_delete = True
+            relation.save()
+            return Response(status=HTTP_204_NO_CONTENT)
+        else:
+            return JsonResponse({
+                'code': 10004,
+                'msg': '用户不存在！'
+            })
 
 
 
