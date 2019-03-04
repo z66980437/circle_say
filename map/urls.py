a160acce0caeca26cc5d74cb2267b8934dfdf974
yuxiaoyu <1044869221@qq.com>
2019-03-04 11:48:58 +0800

    增加了首页按圈子打卡数或者系统热度分页查景点功能，增加了用户所在圈子查询功能

--- map/urls.py
+++ map/urls.py
@@ -1,7 +1,7 @@
 from django.urls import path
 
 from map import views
-from map.views import CityView, RegionView, ScenicSimpleView, cal_hot
+from map.views import CityView, RegionView, ScenicSimpleView, cal_hot, CircleView
 
 urlpatterns = [
     # 测试
@@ -10,4 +10,6 @@ urlpatterns = [
     path('regions/', RegionView.as_view(), name='regions'),
     path('simple_scenics/', ScenicSimpleView.as_view(), name='simple_scenics'),
     path('cal_hot/', cal_hot, name='cal_hot'),
+    path('circles/', CircleView.as_view(), name='circles'),
+    path('circles/<int:pk>/', CircleView.as_view(), name='circle'),
 ]
---
+++ sql_files/user_circle.sql
@@ -0,0 +1,4 @@
+insert into user_circle (circle_id, user_id)
+values (4, 9),
+ (4, 10),
+ (2, 9);
